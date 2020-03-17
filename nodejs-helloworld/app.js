27450b239edb01fb1d0b7c7927d9759c85985e45
woyaowoyao <laidongshi@gmail.com>
2020-03-17 19:16:13 +0800

    Update app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World! test.do288 charpter1 souce-build demo\n');
 });
 
 app.listen(8080, function () {
